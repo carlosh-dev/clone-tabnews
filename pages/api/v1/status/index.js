de2f9eb87559005fab85b310b6287bfe795e8ff1
Carlos Henrique <56794739+carlosh-dev@users.noreply.github.com>
2024-11-19 22:43:54 +0000

    finish `/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,28 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum;")
-  console.log(result.rows)
-  response.status(200).json({ status: 'OK'})
+  
+  const updatedAt = new Date().toISOString();
+
+  const databateVersionResult = await database.query("SHOW server_version;");
+  const version = databateVersionResult.rows[0].server_version;
+  
+  const databateMaxConnextionsResult = await database.query("SHOW max_connections;");
+  const maxConnections = databateMaxConnextionsResult.rows[0].max_connections;
+
+  const databateOpennedConnextionsResult = await database.query("SELECT COUNT(*)::int FROM pg_stat_activity  WHERE datname = 'local_bd';");
+  const databaseOpenConnectionsValue = databateOpennedConnextionsResult.rows[0].count;
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependecies: {
+      database: {
+        max_connections: parseInt(maxConnections),
+        open_connections: databaseOpenConnectionsValue,
+        version: version
+      }
+    }
+  })
 }
 
 export default status;
