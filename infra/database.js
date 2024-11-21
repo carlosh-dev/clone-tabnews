e2153df3add0d20318a4a90850b1e4e6b2359565
Carlos Henrique <56794739+carlosh-dev@users.noreply.github.com>
2024-11-21 22:11:41 +0000

    handle database ssl connection

--- infra/database.js
+++ infra/database.js
@@ -6,7 +6,8 @@ async function query(queryObject) {
     port: process.env.POSTGRES_PORT,
     database: process.env.POSTGRES_DB,
     user: process.env.POSTGRES_USER,
-    password: process.env.POSTGRES_PASSWORD
+    password: process.env.POSTGRES_PASSWORD,
+    ssl: process.env.NODE_ENV === "development" ? false : true
   });
   
   try{
