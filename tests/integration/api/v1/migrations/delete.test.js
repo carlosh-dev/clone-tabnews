dc8ed114fcd474ad3d187057ea637f4e76b164d6
Carlos Henrique <56794739+carlosh-dev@users.noreply.github.com>
2024-12-04 21:50:58 +0000

    add new test to `DELETE` fetches

---
+++ tests/integration/api/v1/migrations/delete.test.js
@@ -0,0 +1,11 @@
+test("DELETE to /api/v1/migrations should not to be allowed", async () => {
+  const response = await fetch("http://localhost:3000/api/v1/migrations", {
+    method: "DELETE",
+  });
+
+  expect(response.status).toBe(405);
+
+  const responseBody = await response.json();
+
+  expect(responseBody.message).toBe(`Method "DELETE" now allowed!`);
+});
