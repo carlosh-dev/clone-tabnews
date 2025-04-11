6af3f5b71cc379e0bb72a6763b6c26252a1906ee
Carlos Henrique <caique.h@hotmail.com>
2025-04-11 15:32:45 -0300

    standardize code formatting and improve readability across multiple files

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,10 +1,10 @@
-import orchestrator from "tests/orchestrator"
+import orchestrator from "tests/orchestrator";
 
-beforeAll(async () => await orchestrator.waitForAllServices())
+beforeAll(async () => await orchestrator.waitForAllServices());
 
 test("GET to /api/v1/status should returns 200", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/status")
-  expect(response.status).toBe(200)
+  const response = await fetch("http://localhost:3000/api/v1/status");
+  expect(response.status).toBe(200);
 
   const responseBody = await response.json();
 
@@ -14,4 +14,4 @@ test("GET to /api/v1/status should returns 200", async () => {
   expect(responseBody.dependecies.database.version).toEqual("16.2");
   expect(responseBody.dependecies.database.max_connections).toEqual(100);
   expect(responseBody.dependecies.database.open_connections).toEqual(1);
-})
+});
