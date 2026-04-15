6d9007910c7ca0cbc5bc12a6d4d02efb8ce8a37b
Carlos <caique.h@hotmail.com>
2026-04-15 19:07:19 -0300

    feat: implement error handlers on `status` controller and add `post.test.js`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,45 +1,59 @@
+import { createRouter } from "next-connect";
 import database from "infra/database.js";
-import { InternalServerError } from "infra/errors";
+import { InternalServerError, MethosNotAllowedError } from "infra/errors";
 
-async function status(request, response) {
-  try {
-    const updatedAt = new Date().toISOString();
+const router = createRouter();
 
-    const databateVersionResult = await database.query("SHOW server_version;");
-    const version = databateVersionResult.rows[0].server_version;
+router.get(getHandler);
 
-    const databateMaxConnectionsResult = await database.query(
-      "SHOW max_connections;",
-    );
-    const maxConnections = databateMaxConnectionsResult.rows[0].max_connections;
+export default router.handler({
+  onNoMatch: onNoMatchHandler,
+  onError: onErrorHandler,
+});
 
-    const databateOpennedConnextionsResult = await database.query(
-      `SELECT COUNT(*)::int FROM pg_stat_activity  WHERE datname = '${process.env.POSTGRES_DB}';`,
-    );
-    const databaseOpenConnectionsValue =
-      databateOpennedConnextionsResult.rows[0].count;
-
-    response.status(200).json({
-      updated_at: updatedAt,
-      dependecies: {
-        database: {
-          max_connections: parseInt(maxConnections),
-          open_connections: databaseOpenConnectionsValue,
-          version: version,
-        },
-      },
-    });
-  } catch (error) {
-    const customErrorObject = new InternalServerError({
-      cause: error,
-    });
-    console.log("\n Erro dentro do controller:");
-    console.log(customErrorObject);
-
-    response.status(500).json({
-      error: customErrorObject,
-    });
-  }
+function onNoMatchHandler(request, response) {
+  const publicErrorObject = new MethosNotAllowedError();
+  response.status(publicErrorObject.statusCode).json(publicErrorObject);
 }
 
-export default status;
+function onErrorHandler(error, request, response) {
+  const customErrorObject = new InternalServerError({
+    cause: error,
+  });
+
+  console.log("\n Erro dentro do next-connect:");
+  console.log(customErrorObject);
+
+  response.status(500).json({
+    error: customErrorObject,
+  });
+}
+
+async function getHandler(request, response) {
+  const updatedAt = new Date().toISOString();
+
+  const databateVersionResult = await database.query("SHOW server_version;");
+  const version = databateVersionResult.rows[0].server_version;
+
+  const databateMaxConnectionsResult = await database.query(
+    "SHOW max_connections;",
+  );
+  const maxConnections = databateMaxConnectionsResult.rows[0].max_connections;
+
+  const databateOpennedConnextionsResult = await database.query(
+    `SELECT COUNT(*)::int FROM pg_stat_activity  WHERE datname = '${process.env.POSTGRES_DB}';`,
+  );
+  const databaseOpenConnectionsValue =
+    databateOpennedConnextionsResult.rows[0].count;
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependecies: {
+      database: {
+        max_connections: parseInt(maxConnections),
+        open_connections: databaseOpenConnectionsValue,
+        version: version,
+      },
+    },
+  });
+}
